fcf624620e4c2bf14ec3ba70599d1f3676643624
johnnyjanu <juni20030703@gmail.com>
2025-02-10 21:12:57 +0900

    [권혁준] - Spring test 프로젝트 생성

---
+++ src/test/java/com/maeun/spring/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maeun.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
