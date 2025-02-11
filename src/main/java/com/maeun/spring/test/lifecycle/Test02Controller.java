12adb55b76caf9070e5f6ac6cecc2fc565066313
johnnyjanu <juni20030703@gmail.com>
2025-02-11 17:11:28 +0900

    [권혁준] - lifecycle test2 완료

--- src/main/java/com/maeun/spring/test/lifecycle/Test02Controller.java
+++ src/main/java/com/maeun/spring/test/lifecycle/Test02Controller.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,16 +51,31 @@ public class Test02Controller {
 		return movieList;
 	}
 	
-	public void printJson2() {
+	//2. json 출력(List, Class)
+	@RequestMapping("/2")
+	public List<Review> printJson2() {
 		
 		Review hagulu = new Review("안녕하세요, 가입인사 드립니다.", "hagulu", "안녕하세요. 가입했어요, 앞으로 잘 부탁 드립니다. 활동 열심히 하겠습니다.");
 		Review bada = new Review("헐 대박", "bada", "오늘 목요일이 었어... 금요일인줄");
 		Review dulumary = new Review("오늘 데이트 한 이야기 해드릴게요", "dulumary", "....");
-		
-		
+		List<Review> reviewList = new ArrayList<>();
+		reviewList.add(hagulu);
+		reviewList.add(bada);
+		reviewList.add(dulumary);
+		return reviewList;
 		
 	}
 	
+	//3. ResponseEntity
+	@RequestMapping("/3")
+	public ResponseEntity<Review> entityResponse() {
+		
+		Review hagulu = new Review("안녕하세요, 가입인사 드립니다.", "hagulu", "안녕하세요. 가입했어요, 앞으로 잘 부탁 드립니다. 활동 열심히 하겠습니다.");
+		
+		ResponseEntity<Review> entity = new ResponseEntity<>(hagulu, HttpStatus.INTERNAL_SERVER_ERROR);
+		return entity;
+		
+	}
 	
 	
 }
