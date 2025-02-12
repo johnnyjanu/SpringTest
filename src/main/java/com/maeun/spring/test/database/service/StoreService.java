0e6d14b191bf7f3f258f14744530f9d693b4ac88
johnnyjanu <juni20030703@gmail.com>
2025-02-12 21:26:25 +0900

    [권혁준] - database 연동(mybatis) 연습문제 완료

---
+++ src/main/java/com/maeun/spring/test/database/service/StoreService.java
@@ -0,0 +1,22 @@
+package com.maeun.spring.test.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maeun.spring.test.database.domain.Store;
+import com.maeun.spring.test.database.repository.StoreRepository;
+
+@Service
+public class StoreService {
+	
+	@Autowired
+	private StoreRepository storeRepository;
+	
+	public List<Store> getStoreList() {
+		List<Store> storeList = storeRepository.selectStoreList();
+		return storeList;
+	}
+	
+}
